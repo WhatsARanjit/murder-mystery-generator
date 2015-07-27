440129581a20596b268d22f672fcfb92764e73b8
Ranjit Viswakumar <whatsaranjit@gmail.com>
2015-07-27 21:41:40 +0000

    Loading a hash and setting default game.yaml

--- lib/murder/action/generate/characters.rb
+++ lib/murder/action/generate/characters.rb
@@ -1,4 +1,5 @@
 require 'murder/util/setopts'
+require 'murder/util/yaml'
 require 'pry'
 
 module MURDER
@@ -7,6 +8,7 @@ module MURDER
       class Characters
 
         include MURDER::Util::Setopts
+        include MURDER::Util::Yaml
 
         def initialize(opts, argv, cmd)
           @opts = opts
@@ -15,11 +17,16 @@ module MURDER
             :config     => :self,
             :trace      => :self
           })
+
+          # Setup defaults
+          @config ||= "#{Dir.pwd}/game.yaml"
+          @config_hash = yaml(@config)
+
         end
 
         def call
           binding.pry
-          puts 'Hello'
+          puts @config_hash['name']
         end
       end
     end
---
+++ lib/murder/util/yaml.rb
@@ -0,0 +1,18 @@
+require 'yaml'
+
+module MURDER
+  module Util
+    module Yaml
+
+      private
+
+      def yaml(file_path)
+        YAML.load_file(file_path)
+        rescue Exception => err
+          puts "YAML invalid: #{file_path}"
+          raise "#{err}"
+      end
+
+    end
+  end
+end
